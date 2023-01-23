17583281a840d4e085b16a05f0ad582f67b3bcff
mona <mo.na.di.87.de@gmail.com>
2023-01-24 01:23:06 +0300

    add display temp in js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  console.log(response.data.temperature.current);
+}
 
+let Key = "e30f73b3544toc0d6faf9afc4179ef7e";
+
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=paris&key=e30f73b3544toc0d6faf9afc4179ef7e&units=metric";
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
