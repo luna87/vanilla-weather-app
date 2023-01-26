41b06b673129539c065d8030a8c20dcc56818c12
mona <mo.na.di.87.de@gmail.com>
2023-01-27 00:12:27 +0300

    update Time

--- src/app.js
+++ src/app.js
@@ -1,19 +1,42 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Tursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours} ${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
-  let cityElement=document.querySelector("#city");
-  let descriptionElement=document.querySelector("#description");
-  let humdityElement=document.querySelector("#humdity");
-  let windElement=document.querySelector("#wind");
-  
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humdityElement = document.querySelector("#humdity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
 
-
-  
-  windElement.innerHTML=response.data.wind.speed;
+  windElement.innerHTML = response.data.wind.speed;
   humdityElement.innerHTML = response.data.temperature.humidity;
   cityElement.innerHTML = response.data.city;
-  descriptionElement.innerHTML=response.data.condition.description;
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-  console.log(response);}
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+}
 
 let Key = "e30f73b3544toc0d6faf9afc4179ef7e";
 
